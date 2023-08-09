ac7b6a144b6a313065aca7ed5e31102abf06751f
Aayush Kantak <120012051+aysh01@users.noreply.github.com>
2023-08-09 16:59:58 +0530

    Update assgn-30.py

--- assgn-30.py
+++ assgn-30.py
@@ -8,3 +8,14 @@ plt.pie(x='Gold',labels='Country',data=a,
         autopct='%1.2f%%')
 plt.title('Gold Medal won by Top 5 Countries in 2016')
 plt.show()
+
+# Here, Pandas has been used to read a CSV file ('medals.csv').
+# An, Excel Sheet is created with columns ['Country','Gold'].
+   Country     || Gold
+United_States  ||  46
+Great Britain  ||  27
+China	       ||  26
+Russia	       ||  19
+Germany	       ||  17
+
+# Save, the above file in same directory with .CSV extension !
